51e532f74b1231d4a1fa9af1c8247876be62933c
zinahoque11 <zinahoque11@gmail.com>
2026-07-26 22:49:09 +0600

    Supplier goal 5&6 added

---
+++ src/main/java/csc305/group57_finalproject_fishhatcheryfarm/Zina_2430851/reportSupplyShortageController.java
@@ -0,0 +1,36 @@
+package csc305.group57_finalproject_fishhatcheryfarm.Zina_2430851;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class reportSupplyShortageController
+{
+    @javafx.fxml.FXML
+    private ComboBox<String> supplyCategoryCB;
+    @javafx.fxml.FXML
+    private DatePicker deliveryDateDP;
+    @javafx.fxml.FXML
+    private TextArea reasonsForShortageTA;
+    @javafx.fxml.FXML
+    private TextField availableQuantityTF;
+    @javafx.fxml.FXML
+    private TextField requestedQuantityTF;
+    @javafx.fxml.FXML
+    private DatePicker restockDateDP;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+        supplyCategoryCB.getItems().addAll("Fish Feed","Medication","Equipment");
+    }
+
+    @javafx.fxml.FXML
+    public void backButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void submitReportButton(ActionEvent actionEvent) {
+    }
+}
