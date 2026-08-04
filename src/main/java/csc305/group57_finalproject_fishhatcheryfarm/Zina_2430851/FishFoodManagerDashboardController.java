247eb040e49ce4f9499d45da32fec2558dc6726e
zinahoque11 <zinahoque11@gmail.com>
2026-08-05 01:44:24 +0600

    Renamed fish food manager's 4th goal fxml & controller

--- src/main/java/csc305/group57_finalproject_fishhatcheryfarm/Zina_2430851/FishFoodManagerDashboardController.java
+++ src/main/java/csc305/group57_finalproject_fishhatcheryfarm/Zina_2430851/FishFoodManagerDashboardController.java
@@ -23,7 +23,7 @@ public class FishFoodManagerDashboardController
     @javafx.fxml.FXML
     public void updateFoodIntake(ActionEvent actionEvent) {
         SceneSwitcher.switchScene(actionEvent,
-                "/csc305/group57_finalproject_fishhatcheryfarm/Zina_2430851/updateFoodIntake.fxml",
+                "/csc305/group57_finalproject_fishhatcheryfarm/Zina_2430851/recordFeedSessions.fxml",
                 "Update Food Intake Section");
     }
 
