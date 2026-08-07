13f12f2755ed0ba6b512a96a68986a86eec12900
ArhamHaider25 <arhamhaider17@gmail.com>
2026-08-07 21:30:33 +0600

    Arham

---
+++ src/main/java/csc305/group57_finalproject_fishhatcheryfarm/Arham_2520927/AccountantDashboardController.java
@@ -0,0 +1,34 @@
+package csc305.group57_finalproject_fishhatcheryfarm.Arham_2520927;
+
+import javafx.event.ActionEvent;
+
+public class AccountantDashboardController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void salarymanagementDBbutton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void addexpenseDBDbutton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void bugetplanningDBbutton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void TaxmanagementDBbutton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void inventorycostDBbutton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void generatereportDBbutton(ActionEvent actionEvent) {
+    }
+}
